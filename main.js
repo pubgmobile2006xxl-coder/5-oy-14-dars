509072b2ba9b367bff812e9a6ce90dcc29c23900
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-04-17 11:40:32 +0500

    js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const suvdata = [
     {id: 1, img:"./images/Fire.png", name: AKSIYA},
+    {id: 2, img:"./images/Pizza.png", name: PIZZA},
+    {id: 3, img:"./images/Sushi.png", name: SUSHI},
+    {id: 4, img:"./images/Drink.png", name: NAPITKI},
+    {id: 5, img:"./images/Snacks.png", name: FRRI},
+    {id: 6, img:"./images/Combo.png", name: COMBO},
+    {id: 7, img:"./images/Dessert.png", name: DESSERT},
+    {id: 8, img:"./images/Sauce.png", name: SOUS},
 ]
