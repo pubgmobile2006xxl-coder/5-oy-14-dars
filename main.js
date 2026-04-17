8eefb5ea5e2f1aaa0fe15f88e3c24e578cbd0a12
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-04-17 22:25:34 +0500

    tugadi

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const headerContainer = document.getElementById("hearder_bottom");
 const headerRasmContainer = document.querySelector(".header_rasm");
 const box = document.getElementById("inp");
+const mainContainer = document.getElementById("pizza_list");
 
 const header_bottom_data = [
     { id: 1, img: "./img/Fire.png", name: "AKSIYA" },
@@ -20,51 +21,15 @@ const header_rasm = [
     { id: 4, img: "./img/Group 82.png", name: "AKSIYA" },
 ];
 
-function rendercard(data, element) {
-    if (!element) return;
 
-    if (data.length === 0) {
-        element.innerHTML = "<h3> Hech narsa yo'q</h3>";
-        return;
-    }
-
-    const cardHTML = data.map(el => `
-    <div class="card"> 
-        <img src="${el.img}" alt="${el.name}">
-        <h1>${el.name}</h1>
-    </div>
-    `).join('');
-
-    element.innerHTML = cardHTML;
-}
-
-rendercard(header_bottom_data, headerContainer);
-rendercard(header_rasm, headerRasmContainer);
-
-function filterInp(data) {
-    if (!box) return;
-
-    box.addEventListener("input", (e) => {
-        let inputValue = e.target.value.toLowerCase();
-
-        let filteredData = data.filter(item => {
-            return item.name.toLowerCase().includes(inputValue);
-        });
-
-        rendercard(filteredData, headerContainer);
-    });
-}
-
-// PIZZA bo'limi
-
-const mainContainer = document.getElementById("pizza_list");
+// PIZZA BOLIM
 
 let pizza_data = [
     {
         id: 1,
         img: "./img/Rectangle 4.png",
         name: "EASY PEASY огуречный расколбас",
-        description: "Курица, Лук, Перец Халапеньо, фирменный соус...",
+        description: "Курица, Лук, Перец Халапеньо,",
         price: 399,
         count: 0,
         badge: "ХИТ"
@@ -134,8 +99,28 @@ let pizza_data = [
     },
 ];
 
+
+function rendercard(data, element) {
+    if (!element) return;
+    if (data.length === 0) {
+        element.innerHTML = "<h3> Hech narsa yo'q</h3>";
+        return;
+    }
+    const cardHTML = data.map(el => `
+    <div class="card"> 
+        <img src="${el.img}" alt="${el.name}">
+        <h1>${el.name}</h1>
+    </div>
+    `).join('');
+    element.innerHTML = cardHTML;
+}
+
 function renderPizzas(data) {
     if (!mainContainer) return;
+    if (data.length === 0) {
+        mainContainer.innerHTML = "<h3 style='grid-column: 1/-1; text-align: center;'>Hech narsa topilmadi...</h3>";
+        return;
+    }
 
     mainContainer.innerHTML = data.map(item => `
         <div class="card">
@@ -148,21 +133,39 @@ function renderPizzas(data) {
                 <button onclick="changeCount(${item.id}, -1)">-</button>
                 <span>${item.count}</span>
                 <button onclick="changeCount(${item.id}, 1)">+</button>
-            </div>
 
             <div class="card-footer">
                 <button class="select-btn" onclick="resetCount(${item.id})">Выбрать</button>
                 <span class="price">от ${item.price * item.count} ₽</span>
             </div>
+            </div>
+
         </div>
     `).join('');
 }
+
+// input bolim
+
+function filterInp(data) {
+    if (!box) return;
+
+    box.addEventListener("input", (e) => {
+        let inputValue = e.target.value.toLowerCase();
+
+        let filteredData = data.filter(item => {
+            return item.name.toLowerCase().includes(inputValue);
+        });
+
+        renderPizzas(filteredData);
+    });
+}
+
 
 window.changeCount = function (id, action) {
     pizza_data = pizza_data.map(item => {
         if (item.id === id) {
             let newCount = item.count + action;
-            return { ...item, count: newCount < 1 ? 1 : newCount };
+            return { ...item, count: newCount < 0 ? 0 : newCount };
         }
         return item;
     });
@@ -179,4 +182,10 @@ window.resetCount = function (id) {
     renderPizzas(pizza_data);
 };
 
+rendercard(header_bottom_data, headerContainer);
+rendercard(header_rasm, headerRasmContainer);
 renderPizzas(pizza_data);
+filterInp(pizza_data);
+
+// SUSHI BOLIM
+
